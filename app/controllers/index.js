c58236673f0fc56007d245b2ef3a5d1b85325fc3
Lauren Barker <ms.lebarker@gmail.com>
2017-11-02 17:22:19 -0400

    Use tasks on dashboard

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,28 +2,44 @@ import { computed } from '@ember/object';
 import { notEmpty } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
+import { task, timeout } from 'ember-concurrency';
 
 export default Controller.extend({
     session: service(),
     currentUser: service(),
 
-    queryParams: ['page'],
-    page: 1,
+    modelName: 'preprint-provider',
+    query: {
+        'filter[permissions]': 'view_actions,set_up_moderation',
+    },
+    data: [],
+
+    init() {
+        this._super(...arguments);
+        this.get('fetchData').perform();
+    },
+
+    fetchData: task(function* () {
+        yield timeout(1000);
+        const results = yield this.get('store')
+            .query(this.get('modelName'), this.get('query'));
+        this.set('data', results);
+    }).restartable(),
 
     showSetup: notEmpty('providersToSetup'),
 
-    providersToSetup: computed('model.[]', function() {
-        return this.get('model').filter(provider =>
+    providersToSetup: computed('data.[]', 'fetchData.isRunning', function() {
+        return this.get('data').filter(provider =>
             !provider.get('reviewsWorkflow') && provider.get('permissions').includes('set_up_moderation'));
     }),
 
-    sidebarProviders: computed('model.[]', function() {
-        return this.get('model').filter(provider =>
+    sidebarProviders: computed('data.[]', 'fetchData.isRunning', function() {
+        return this.get('data').filter(provider =>
             provider.get('reviewsWorkflow') || provider.get('permissions').includes('set_up_moderation'));
     }),
 
-    showDashboard: computed('model.[]', function() {
-        const providers = this.get('model');
+    showDashboard: computed('data.[]', 'fetchData.isRunning', function() {
+        const providers = this.get('data');
         return providers.any(p => p.get('reviewsWorkflow'));
     }),
 
