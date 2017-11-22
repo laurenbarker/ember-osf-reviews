611fd5748e6fd0265a4e939a773050c903e2f598
Lauren Barker <ms.lebarker@gmail.com>
2017-11-22 15:37:41 -0500

    Remove excess request

--- app/components/moderation-base/component.js
+++ app/components/moderation-base/component.js
@@ -2,7 +2,6 @@ import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 
-import { task } from 'ember-concurrency';
 /**
  * The base for the provider moderation page (tabs, provider name, and breadcrumbs).
  *
@@ -33,26 +32,5 @@ export default Component.extend({
 
     didReceiveAttrs() {
         this.set('providerName', this.get('theme.provider.name'));
-        this.get('fetchData').perform();
     },
-
-    fetchData: task(function* () {
-        try {
-            const response = yield this.get('store').findRecord(
-                'preprint-provider',
-                this.get('theme.id'), {
-                    adapterOptions: {
-                        query: { related_counts: true },
-                    },
-                    reload: true,
-                },
-            );
-            this.get('theme').set(
-                'reviewableStatusCounts',
-                response.get('reviewableStatusCounts'),
-            );
-        } catch (e) {
-            this.transitionToRoute('page-not-found');
-        }
-    }),
 });
